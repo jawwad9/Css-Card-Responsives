c84f1aa0bc2468addd7dd6ea2894b698c89db151
jawwad9 <jawwadmuhammad514@gmail.com>
2025-10-06 14:13:58 -0700

    CRUD Operations and Local Storag

---
+++ CRUD Operations and Local Storage/script.js
@@ -0,0 +1,49 @@
+const tableData = document.querySelector(".data-table")
+const createData = document.querySelector(".create-form")
+
+let data = [
+    {id: 1, name: "anas", email: "anas@gmail.com"},
+    {id: 2, name: "bilal", email: "bilal@gmail.com"}
+]
+
+function readAll(){
+    localStorage.setItem("object", JSON.stringify(data));
+    let object = localStorage.getItem('object')
+    let objectData = JSON.parse(object);
+    let elements = "" ;
+    console.log(objectData);
+    
+
+    objectData.map((record)=>{
+        elements += `<tr>
+        <td>${record.name}</td>
+        <td>${record.email}</td>
+        <td>
+        <button>Edit</button>
+        <button>Delete</button>
+        </td>
+        <tr/>`
+    })
+
+    tableData.innerHTML = elements;
+}
+
+function create(){
+    createData.style.display = 'block';
+    addBtn.style.display = 'none';
+    console.log("create function");
+    
+}
+
+function add(){
+    const name = document.querySelector(".name").value;
+    const email = document.querySelector(".email").value;
+
+    let newObj = {id: 3, name: name, email: email};
+    data.push(newObj);
+
+    readAll();
+
+    createData.style.display = 'none';
+    addBtn.style.display = 'block';
+}
